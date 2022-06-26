51d09444674447a791b40917c6e8e4334df6c705
Sarah Schulte <git@sarah.engineer>
2022-06-26 16:50:02 -0700

    add missing hover check to interactive effect
    Previously, setting 'interactive' to true would inadvertently add mouseenter / mouseleave listeners to the tooltip. This makes interactive tooltips with other triggers behave as though they have the 'hover' trigger.
    
    After this change, tooltips that are not triggered by 'hover' will no longer receive register these listeners.

--- src/usePopperTooltip.ts
+++ src/usePopperTooltip.ts
@@ -202,7 +202,7 @@ export function usePopperTooltip(
 
   // Trigger: hover on tooltip, keep it open if hovered
   React.useEffect(() => {
-    if (tooltipRef == null || !getLatest().finalConfig.interactive) return;
+    if (tooltipRef == null || !isTriggeredBy('hover') || !getLatest().finalConfig.interactive) return;
 
     tooltipRef.addEventListener('mouseenter', showTooltip);
     tooltipRef.addEventListener('mouseleave', hideTooltip);
